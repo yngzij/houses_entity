f7d6a6c2c877cc0784e264d45c7a7e52644d6bea
yngzij <yngzij@gmail.com>
2021-06-16 02:26:33 +0800

    add 'house'

--- house.go
+++ house.go
@@ -1,1 +1,19 @@
 package houses_entity
+
+import "time"
+
+type House struct {
+	Id       int       `gorm:"id"`
+	Price    int       `gorm:"price"`
+	ImageUrl string    `gorm:"image_url"`
+	Title    string    `gorm:"title"`
+	Sort     int       `gorm:"sort"`
+	Desc     string    `gorm:"desc"`
+	Deleted  time.Time `gorm:"deleted"`
+	Updated  time.Time `gorm:"updated"`
+	Created  time.Time `gorm:"created"`
+}
+
+func (h House) TableName() string {
+	return "houses"
+}
