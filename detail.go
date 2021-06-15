f8cf655fb9ea2cc8374bd6b7c667b63cea8a2153
yngzij <yngzij@gmail.com>
2021-06-16 02:26:56 +0800

    add 'detail'

---
+++ detail.go
@@ -0,0 +1,17 @@
+package houses_entity
+
+import "time"
+
+type Detail struct {
+	Id        int       `gorm:"id"`
+	ImagesURL string    `gorm:"images_url"`
+	HouseId   int       `gorm:"house_id"`
+	Deleted   time.Time `gorm:"deleted"`
+	Sort      int       `gorm:"sort"`
+	Created   time.Time `gorm:"created"`
+	Updated   time.Time `gorm:"updated"`
+}
+
+func (d Detail) TableName() string {
+	return "details"
+}
