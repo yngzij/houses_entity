559cb18616b6654779af51b2a2423dd47b4f6a6d
yngzij <yngzij@gmail.com>
2021-06-17 00:00:58 +0800

    change 'images_url' to 'image_url'

--- detail.go
+++ detail.go
@@ -3,13 +3,13 @@ package houses_entity
 import "time"
 
 type Detail struct {
-	Id        int       `gorm:"id"`
-	ImagesURL string    `gorm:"images_url"`
-	HouseId   int       `gorm:"house_id"`
-	Deleted   time.Time `gorm:"deleted"`
-	Sort      int       `gorm:"sort"`
-	Created   time.Time `gorm:"created"`
-	Updated   time.Time `gorm:"updated"`
+	Id       int       `gorm:"id"`
+	ImageURL string    `gorm:"image_url"`
+	HouseId  int       `gorm:"house_id"`
+	Deleted  time.Time `gorm:"deleted"`
+	Sort     int       `gorm:"sort"`
+	Created  time.Time `gorm:"created"`
+	Updated  time.Time `gorm:"updated"`
 }
 
 func (d Detail) TableName() string {
