4df30ba002cd8e512d9f55dc4d2e6937b964f470
yngzij <yngzij@gmail.com>
2021-06-29 20:10:46 +0800

    feat: add 'User'

--- user.go
+++ user.go
@@ -1,1 +1,19 @@
 package houses_entity
+
+import "time"
+
+type User struct {
+	Id        int       `gorm:"id"`
+	AdvertId  int       `gorm:"advert_id"`
+	Avatar    string    `gorm:"avatar"`
+	NickName  string    `gorm:"nick_name"`
+	RealName  string    `gorm:"real_name"`
+	Local     string    `gorm:"local"`
+	DeletedAt time.Time `gorm:"deleted_at"`
+	UpdatedAt time.Time `gorm:"updated_at"`
+	CreatedAt time.Time `gorm:"created_at"`
+}
+
+func (u User) TableName() string {
+	return "users"
+}
