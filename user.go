02345ec70648b11f2ffb2570807e918fbfa72ca0
yngzij <yngzij@gmail.com>
2021-06-29 20:22:18 +0800

    fix: location

--- user.go
+++ user.go
@@ -8,7 +8,7 @@ type User struct {
 	Avatar    string    `gorm:"avatar"`
 	NickName  string    `gorm:"nick_name"`
 	RealName  string    `gorm:"real_name"`
-	Local     string    `gorm:"local"`
+	Location  string    `gorm:"location"`
 	DeletedAt time.Time `gorm:"deleted_at"`
 	UpdatedAt time.Time `gorm:"updated_at"`
 	CreatedAt time.Time `gorm:"created_at"`
