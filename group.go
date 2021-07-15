b2e29036544b44d35f31645ff97d574157155987
yngzij <yngzij@gmail.com>
2021-07-15 23:52:40 +0800

    fix: advert

--- group.go
+++ group.go
@@ -4,4 +4,9 @@ type Group struct {
 	Id          int    `gorm:"id"`
 	Phone       string `gorm:"phone"`
 	ChatGroupId string `gorm:"chat_group_id"`
+	AdvertId    int    `gorm:"advert_id"`
+}
+
+func (g Group) TableName() string {
+	return "groups"
 }
