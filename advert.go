e9ef0cb62028e197a1ac0394b58a6f42e96dc7c6
yngzij <yngzij@gmail.com>
2021-07-17 01:49:29 +0800

    fix:

--- advert.go
+++ advert.go
@@ -9,25 +9,25 @@ const (
 )
 
 type Advert struct {
-	Id        int        `gorm:"id"`
-	UsedNum   int        `gorm:"used_num"`
-	Avatar    string     `gorm:"avatar"`
-	NickName  string     `gorm:"nick_name"`
-	Title     string     `gorm:"title"`
-	UnusedNum int        `gorm:"unused_num"`
-	UserId    int        `gorm:"user_id"`
-	OpenId    string     `gorm:"open_id"`
-	GroupId   string     `gorm:"group_id"`
-	TotalNum  int        `gorm:"total_num"`
-	Desc      string     `gorm:"desc"`
-	Status    int        `gorm:"status"`
-	Sort      int        `gorm:"sort"`
-	DeletedAt *time.Time `gorm:"deleted_at"`
-	UpdatedAt time.Time  `gorm:"updated_at"`
-	CreatedAt time.Time  `gorm:"created_at"`
-	Phone     string     `gorm:"phone"`
-	StartTime int64      `gorm:"start_time"`
-	EndTime   int64      `gorm:"end_time"`
+	Id          int        `gorm:"id"`
+	UsedNum     int        `gorm:"used_num"`
+	Avatar      string     `gorm:"avatar"`
+	NickName    string     `gorm:"nick_name"`
+	Title       string     `gorm:"title"`
+	UnusedNum   int        `gorm:"unused_num"`
+	ChatGroupId string     `gorm:"chat_group_id"`
+	UserId      int        `gorm:"user_id"`
+	OpenId      string     `gorm:"open_id"`
+	TotalNum    int        `gorm:"total_num"`
+	Desc        string     `gorm:"desc"`
+	Status      int        `gorm:"status"`
+	Sort        int        `gorm:"sort"`
+	DeletedAt   *time.Time `gorm:"deleted_at"`
+	UpdatedAt   time.Time  `gorm:"updated_at"`
+	CreatedAt   time.Time  `gorm:"created_at"`
+	Phone       string     `gorm:"phone"`
+	StartTime   int64      `gorm:"start_time"`
+	EndTime     int64      `gorm:"end_time"`
 }
 
 func (a Advert) TableName() string {
--- group.go
+++ group.go
@@ -1,10 +1,16 @@
 package houses_entity
 
+import "time"
+
 type Group struct {
-	Id          int    `gorm:"id"`
-	Phone       string `gorm:"phone"`
-	ChatGroupId string `gorm:"chat_group_id"`
-	AdvertId    int    `gorm:"advert_id"`
+	Id          int        `gorm:"id"`
+	Phone       string     `gorm:"phone"`
+	ChatGroupId string     `gorm:"chat_group_id"`
+	AdvertId    int        `gorm:"advert_id"`
+	OpenId      string     `gorm:"open_id"`
+	DeletedAt   *time.Time `gorm:"deleted_at"`
+	UpdatedAt   time.Time  `gorm:"updated_at"`
+	CreatedAt   time.Time  `gorm:"created_at"`
 }
 
 func (g Group) TableName() string {
