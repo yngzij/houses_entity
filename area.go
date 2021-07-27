f44e25ff306f90d30871a76a849909a8bc4f3961
yngzij <yngzij@gmail.com>
2021-07-27 08:55:25 +0800

    fix:

--- area.go
+++ area.go
@@ -1,6 +1,11 @@
 package houses_entity
 
 type Area struct {
-	Id    int    `gorm:"id"`
-	Value string `gorm:"value"`
+	Id       int    `gorm:"id"`
+	Value    string `gorm:"value"`
+	AdvertId int    `gorm:"advert_id"`
+}
+
+func (Area)TableName() string {
+	return "areas"
 }
