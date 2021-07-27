37cfef4e31cc8f2c36039bda6dd5a959236e006f
yngzij <yngzij@gmail.com>
2021-07-28 00:28:14 +0800

    feat: area

--- area.go
+++ area.go
@@ -1,6 +1,11 @@
 package houses_entity
 
+import "time"
+
 type Area struct {
-	Id    int    `gorm:"id"`
-	Value string `gorm:"value"`
+	Id        int        `gorm:"id"`
+	Value     string     `gorm:"value"`
+	DeletedAt *time.Time `gorm:"deleted_at"`
+	UpdatedAt time.Time  `gorm:"updated_at"`
+	CreatedAt time.Time  `gorm:"created_at"`
 }
