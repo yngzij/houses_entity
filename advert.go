65de39a4f3f783ec2e10f82b1c9841ba94773b54
yngzij <yngzij@gmail.com>
2021-10-03 04:27:49 +0800

    修改审核状态

--- advert.go
+++ advert.go
@@ -3,46 +3,34 @@ package houses_entity
 import (
 	"time"
 
+	"gorm.io/gorm"
+
 	"github.com/lib/pq"
-)
-
-const (
-	AdvertAuditSuccess = 1 // 审核成功
-	AdvertAuditFailure = 2 // 审核失败
-	AdvertAuditing     = 3 // 审核中
-
-	AdvertFnished  = 4 // 完成
-	Adverting      = 5 // 发布中
-	AdvertCanceled = 6 // 取消
-	AdvertDeleted  = 7 // 已经删除
 )
 
 type AdvertStatus int
 
 const (
-	AdventExpired    AdvertStatus = 1
-	AdventPublishing AdvertStatus = 2
-	AdventFinished   AdvertStatus = 3
-	AdventDeleted    AdvertStatus = 4
+	AdvertExpired    AdvertStatus = 1
+	AdvertPublishing AdvertStatus = 2
+	AdvertFinished   AdvertStatus = 3
+	AdvertDeleted    AdvertStatus = 4
 )
 
 type Advert struct {
 	ID          int            `gorm:"id"`
-	UsedNum     int            `gorm:"used_num"`
 	Avatar      string         `gorm:"avatar"`
 	NickName    string         `gorm:"nick_name"`
 	Area        pq.StringArray `gorm:"type:text[]"`
-	Title       string         `gorm:"title"`
 	Local       string         `gorm:"local"`
 	ChatGroupID string         `gorm:"chat_group_id"`
 	UserID      string         `gorm:"user_id"`
-	Permission  int            `gorm:"permission"`
+	Permissions int            `gorm:"permissions"`
 	OpenID      string         `gorm:"open_id"`
-	TotalNum    int            `gorm:"total_num"`
 	Desc        string         `gorm:"desc"`
 	Status      AdvertStatus   `gorm:"status"`
 	Sort        int            `gorm:"sort"`
-	DeletedAt   *time.Time     `gorm:"deleted_at"`
+	DeletedAt   gorm.DeletedAt `gorm:"deleted_at"`
 	UpdatedAt   time.Time      `gorm:"updated_at"`
 	CreatedAt   time.Time      `gorm:"created_at"`
 	Phone       string         `gorm:"phone"`
